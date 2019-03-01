976a556b5f941d97ef5d48701e170913e01eea9f
RomanKopyl <kopylroman@gmail.com>
2019-03-01 07:46:54 +0200

    add onChangeText

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React, {Component} from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Header, DayCard, MainArr } from './src/components/uikit'
+import { StyleSheet, Text, View, TextInput } from 'react-native'
+import { Header, DayCard } from './src/components/uikit'
 
-const url = 'https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22'
+// const url = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=b6907d289e10d714a6e88b30761fae22'
+// const url = 'http://api.openweathermap.org/data/2.5/weather?q=kremenchuk&appid=0d14d606b8980beb4ac945d6bbe071e6'
+const url1 = 'http://api.openweathermap.org/data/2.5/weather?q='
+// const url2 = 'kremenchuk'
+const url3 = '&appid=0d14d606b8980beb4ac945d6bbe071e6'
 export default class App extends Component {
   state = {
     title: 'Weather Forecast',
+    city: 'kremenchuk',
+    url: '',
     data: {
       main: {
         temp: 0
@@ -15,6 +21,8 @@ export default class App extends Component {
 
   componentDidMount = async() => {
     try {
+      const url = url1 + this.state.city + url3
+      console.log('url', this.url);
       const response = await fetch(url)
       const data = await response.json()
       this.setState({ data })
@@ -25,10 +33,16 @@ export default class App extends Component {
 
   render() {
     console.log('state', this.state);
-    const { title, data } = this.state
+    console.log('url', this.url);
+    const { title, city, data } = this.state
     return (
       <View>
         <Header title={title} />
+        <TextInput
+          style={{height: 40}}
+          placeholder="Enter your city."
+          onChangeText={(city) => this.setState({city})}
+        />
         <View>
           <DayCard data={data}/>
         </View>
--- src/components/uikit/DayCard.js
+++ src/components/uikit/DayCard.js
@@ -6,10 +6,12 @@ const DayCard = ({ data }) => {
   const { h1, container } = styles
   const { main: { temp }, name } = data
   console.log('data', data)
+  temp1 = (temp - 273.15).toFixed(1)
+  sign = (temp1 > 0 ? '+' : '')
   return (
     <View style={container}>
       <Text style={h1}>{name}</Text>
-      <Text style={h1}>{temp - 273,15}</Text>
+      <Text style={h1}>{sign}{temp1}</Text>
     </View>
   )
 }
